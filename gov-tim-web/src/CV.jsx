333c8cdcde750ca5c670a145d216ef39cb699e7c
tim <timothee.escandell@gmail.com>
2024-11-08 17:24:37 +0100

    add download button cv

--- gov-tim-web/src/CV.jsx
+++ gov-tim-web/src/CV.jsx
@@ -1,5 +1,15 @@
+import { Button } from "@codegouvfr/react-dsfr/Button"
+
 function CV() {
     return  <div>
+                <br/>
+                <Button
+                    style={{left: "50%", transform: "translate(-50%)",
+                            position: "absolute"}}
+                    linkProps={{href: "https://tim-web.fr/assets/cv.jpg"}}>
+                    Télécharger
+                </Button>
+                <br/><br/><br/>
                 <iframe src="/raw/cv.html" style={{ width: '100vw', height: '100vh' }} />
             </div>
 }
